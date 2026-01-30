4d7030441f997e01d64a78771d249e4ff8a7ae4d
postkevone <49175327+postkevone@users.noreply.github.com>
2026-01-30 22:31:55 +0900

    add toolchain and update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copy the defconfig
+cp -f kernelsu-defconfig/x1q_kor_singlew_defconfig arch/arm64/configs/vendor/x1q_kor_singlew_defconfig
+# Copy the patches
+cp -rf kernelsu-patches/. .
+# Add KernelSU Next legaxy branch
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
 
 export ARCH=arm64
 mkdir out
@@ -11,4 +17,12 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/x1q_kor_singlex_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Build the anykernel flashable zip
+cp out/arch/arm64/boot/Image $(pwd)/anykernel
+cd anykernel && 7z a -tzip ../x1q_ksunext.zip . && cd ..
+
+if [ -f "x1q_ksunext.zip" ]; then
+    echo "Flashable zip file created at: $(pwd)/x1q_ksunext.zip"
+else
+    echo "Error: Zip file was not found!"
+fi
